716333da7b423c2fd97cd7629c76b42eae8724a7
lahu5762 <lars.huson.5762@student.uu.se>
2023-01-24 11:02:18 +0100

    docker image

--- code/batchscripts/bertolotti.sh
+++ code/batchscripts/bertolotti.sh
@@ -8,9 +8,7 @@
 #SBATCH --mail-user lars.huson.5762@student.uu.se
 
 
-module load bioinfo-tools bwa samtools/1.8 snakemake
-module unload python
-module load conda/latest
+module load bioinfo-tools bwa samtools/1.8
 export SRCDIR='/proj/sllstore2017034/nobackup/work/lars/thesis/'
 cp -r $SRCDIR/data $SNIC_TMP
 cp -r $SRCDIR/code $SNIC_TMP
@@ -18,9 +16,11 @@ cd $SNIC_TMP
 
 #python code/python/generate_masked_ranges.py data/raw_data/reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa > Canis_familiaris.Ngaps.bed
 
-snakemake -s code/snakemake/Snakefile -c1 --use-conda --conda-prefix code/snakemake
+smoove call --outdir /proj/sllstore2017/nobackup/work/lars/thesis/out/smoove/results/ --exclude {EXCLUDE} –name {wildcards.sample} --fasta {REF} -p 1 --genotype {input.cram}
+smoove merge --name merged -f {REF} --outdir /proj/sllstore2017/nobackup/work/lars/thesis/out/smoove/results/ {input.vcf}
+smoove genotype -d -x -p 1 --name {wildcards.sample}-joint –outdir /proj/sllstore2017/nobackup/work/lars/thesis/out/smoove/results/genotyped/ --fasta {REF} --vcf {input.merge} {input.cram}
+smoove paste --name cohort {input.vcf}
+smoove annotate --gff {GFF} {input} | bgzip -c > {output}
 
 #cp Canis_familiaris.Ngaps.bed $SRCDIR/out
-cp -r smoove $SRCDIR/out
-cp -r logs $SRCDIR/out
-
+#cp -r logs $SRCDIR/outex
