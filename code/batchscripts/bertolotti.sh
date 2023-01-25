ad8fed01a61feb9729ce9851cf7cdef837bc9a59
lahu5762 <lars.huson.5762@student.uu.se>
2023-01-25 16:10:43 +0100

    bertolotti script

--- code/batchscripts/bertolotti.sh
+++ code/batchscripts/bertolotti.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 #SBATCH -A p2018002
 #SBATCH -p core -n 1
-#SBATCH -t 0-10:00:00
-#SBATCH -J TEST_bertolotti_snakemake 
-#SBATCH -o TEST_bertolotti_snakemake_out.slurm -e TEST_bertolotti_snakemake_error.slurm
+#SBATCH -t 6-0:00:00
+#SBATCH -J TEST_bertolotti 
+#SBATCH -o TEST_bertolotti_out.slurm -e TEST_bertolotti_error.slurm
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user lars.huson.5762@student.uu.se
 
 
-module load bioinfo-tools bwa samtools/1.8
-export SRCDIR='/proj/sllstore2017034/nobackup/work/lars/thesis/'
-cp -r $SRCDIR/data $SNIC_TMP
-cp -r $SRCDIR/code $SNIC_TMP
-cd $SNIC_TMP
+module load bioinfo-tools snakemake
 
-#python code/python/generate_masked_ranges.py data/raw_data/reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa > Canis_familiaris.Ngaps.bed
+# create mask for exclusion during SV calling
+python code/python/generate_masked_ranges.py data/raw_data/reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa > data/bertolotti/Canis_familiaris.Ngaps.bed
 
-smoove call --outdir /proj/sllstore2017/nobackup/work/lars/thesis/out/smoove/results/ --exclude {EXCLUDE} –name {wildcards.sample} --fasta {REF} -p 1 --genotype {input.cram}
-smoove merge --name merged -f {REF} --outdir /proj/sllstore2017/nobackup/work/lars/thesis/out/smoove/results/ {input.vcf}
-smoove genotype -d -x -p 1 --name {wildcards.sample}-joint –outdir /proj/sllstore2017/nobackup/work/lars/thesis/out/smoove/results/genotyped/ --fasta {REF} --vcf {input.merge} {input.cram}
-smoove paste --name cohort {input.vcf}
-smoove annotate --gff {GFF} {input} | bgzip -c > {output}
-
-#cp Canis_familiaris.Ngaps.bed $SRCDIR/out
-#cp -r logs $SRCDIR/outex
+# run SV caller in snakemake
+snakemake -s code/snakemake/Snakefile -c1 --use-singularity -j 64 \
+        --cluster "sbatch -A p2018002 -p {cluster.partition} -n {cluster.n} -t {cluster.time} -e {cluster.error} -o {cluster.output}" \
+        --cluster-config code/snakemake/config_snake_cluster.json
