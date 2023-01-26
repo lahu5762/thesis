2f0602895f958faf903cfe471047d8ce453687fc
lahu5762 <lars.huson.5762@student.uu.se>
2023-01-26 17:36:18 +0100

    add index files

--- code/batchscripts/bertolotti.sh
+++ code/batchscripts/bertolotti.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p2018002
 #SBATCH -p core -n 1
-#SBATCH -t 6-0:00:00
+#SBATCH -t 5-15:00:00
 #SBATCH -J TEST_bertolotti 
 #SBATCH -o TEST_bertolotti_out.slurm -e TEST_bertolotti_error.slurm
 #SBATCH --mail-type=ALL
@@ -11,9 +11,12 @@
 module load bioinfo-tools snakemake
 
 # create mask for exclusion during SV calling
-python code/python/generate_masked_ranges.py data/raw_data/reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa > data/bertolotti/Canis_familiaris.Ngaps.bed
+#python code/python/generate_masked_ranges.py data/raw_data/reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa > data/bertolotti/Canis_familiaris.Ngaps.bed
+
+# create flowchart of snakemake pipeline
+#snakemake --snakefile code/snakemake/Snakefile --dag | dot -Tsvg > snake_dag.svg
 
 # run SV caller in snakemake
-snakemake -s code/snakemake/Snakefile -c1 --use-singularity -j 64 \
-        --cluster "sbatch -A p2018002 -p {cluster.partition} -n {cluster.n} -t {cluster.time} -e {cluster.error} -o {cluster.output}" \
-        --cluster-config code/snakemake/config_snake_cluster.json
+snakemake -s code/snakemake/Snakefile --use-singularity --nolock -j 64 \
+        --cluster "sbatch -A p2018002 -p {cluster.partition} -n {cluster.n} -t {cluster.time} -e {cluster.error}.slurm -o {cluster.output}.slurm" \
+        --cluster-config code/snakemake/config_snake_cluster.json 
