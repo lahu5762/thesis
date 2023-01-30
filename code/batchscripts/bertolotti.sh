e6b4d31af8ed4a0921c4eeadc4450bca0adb4209
lahu5762 <lars.huson.5762@student.uu.se>
2023-01-30 13:44:31 +0100

    test run bertolotti

--- code/batchscripts/bertolotti.sh
+++ code/batchscripts/bertolotti.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A p2018002
 #SBATCH -p core -n 1
-#SBATCH -t 4-22:00:00
+#SBATCH -t 1-18:00:00
 #SBATCH -J TEST_bertolotti 
 #SBATCH -o TEST_bertolotti_out.slurm -e TEST_bertolotti_error.slurm
 #SBATCH --mail-type=ALL
@@ -11,11 +11,13 @@
 module load bioinfo-tools snakemake
 
 # create mask for exclusion during SV calling
-#python code/python/generate_masked_ranges.py data/raw_data/reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa > data/bertolotti/Canis_familiaris.Ngaps.bed
+python code/python/generate_masked_ranges.py data/raw_data/reference/Canis_familiaris.CanFam3.1.dna.toplevel.fa > data/bertolotti/Canis_familiaris.Ngaps.bed
 
 # create flowchart of snakemake pipeline
 #snakemake --snakefile code/snakemake/Snakefile --dag | dot -Tsvg > snake_dag.svg
 
+# prep slurm dir
+mkdir logs/slurm
 # run SV caller in snakemake
 snakemake -s code/snakemake/Snakefile --use-singularity --nolock -j 64 \
         --cluster "sbatch -A p2018002 -p {cluster.partition} -n {cluster.n} -t {cluster.time} -e {cluster.error}.slurm -o {cluster.output}.slurm" \
